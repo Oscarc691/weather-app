926eca061bb02aa2342914981fcd06df36a2a1a8
Oscar Campos <oscarc691@gmail.com>
2023-03-04 21:51:04 -0800

    completely recreated code because i couldnt find the problem before lol

---
+++ Assets/script.js
@@ -0,0 +1,153 @@
+var APIKey = '94f1fc415316d4290d1bcf565d7ea27a';
+var searchBox = document.getElementById('search');
+var newCity = document.getElementById('city');
+var searchBtn = document.getElementById('search-btn');
+var searchHistory = [];
+var btnHolder = document.getElementById('btn-holder');
+var cityName;
+
+//Main Function: 
+load()
+function getCoord() {
+    if(!cityName) {
+        geoCode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchBox.value + '&limit=1&appid=' + APIKey + '';
+        var dynBtn = document.createElement('button');
+        dynBtn.classList.add('dyn-btn')
+        dynBtn.textContent = searchBox.value
+        btnHolder.append(dynBtn);
+        handleHistory(searchBox.value)
+    }else {
+            geoCode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + APIKey + '';
+            handleHistory(cityName)
+        }
+    cityName = ''
+    console.log(searchBox.value)
+    fetch(geoCode)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            var lon = data[0].lon;
+            console.log(lon)
+            var lat = data[0].lat;
+            console.log(lat)
+            queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+            fetch(queryURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data)
+                    newCity.textContent = data.city.name;
+                    console.log(newCity)
+                    
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('temp-' + i + '').textContent = 'Temp: ' + Number(data.list[i].main.temp).toFixed(0) + '°';
+                    }
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('wind-' + i + '').textContent = 'Wind: ' + Number(data.list[i].wind.speed).toFixed(0) + ' mph';
+                    }
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('hum-' + i + '').textContent = 'Humidity: ' + Number(data.list[i].main.humidity) + '%';
+                    }
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('icon-' + i + '').src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png';
+                    }
+
+                })
+        });
+    // handleHistory()
+};
+//Displays days of the week on the forecast
+
+var weekday = [
+    moment().format('dddd'),
+    moment().add(1, 'd').format('dddd'),
+    moment().add(2, 'd').format('dddd'),
+    moment().add(3, 'd').format('dddd'),
+    moment().add(4, 'd').format('dddd'),
+    moment().add(5, 'd').format('dddd'),
+    moment().add(6, 'd').format('dddd'),
+]
+
+for (i = 0; i < 6; i++) {
+    document.getElementById('day-' + i + '').textContent = weekday[i];
+}
+//Event Listeners
+searchBtn.addEventListener('click', getCoord)
+
+//Logs weather forecast based on current location
+window.addEventListener('load', () => {
+    var lat;
+    var lon;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+
+            fetch(queryURL)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    console.log(data)
+                    newCity.textContent = data.city.name;
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('temp-' + i + '').textContent = 'Temp: ' + Number(data.list[i].main.temp).toFixed(0) + '°';
+                    }
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('wind-' + i + '').textContent = 'Wind: ' + Number(data.list[i].wind.speed).toFixed(0) + ' mph';
+                    }
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('hum-' + i + '').textContent = 'Humidity: ' + Number(data.list[i].main.humidity) + '%';
+                    }
+
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById('icon-' + i + '').src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png';
+                    }
+                })
+        });
+    }
+})
+
+// Sets user search into local storage.
+function handleHistory(event) {
+    if (!searchBox.value) {
+        return;
+    };    
+    searchHistory.push(event);
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+};
+
+function load() {
+    var loadHistory = localStorage.getItem('searchHistory')
+    if (loadHistory == null) {
+        return;
+    };
+
+var cityButtonArr = JSON.parse(loadHistory)
+console.log(cityButtonArr)
+for (var i = 0; i < cityButtonArr.length; i++) {
+    var dynBtn = document.createElement('button');
+    dynBtn.classList.add('dyn-btn')
+    dynBtn.textContent = cityButtonArr[i]
+    btnHolder.append(dynBtn);
+}
+};
+
+btnHolder.addEventListener('click', (event)=> {
+    cityName = event.target.textContent;
+    getCoord()
+}) 
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# weather-app
+# 06 Server-Side APIs: Weather Dashboard
+
+## Description
+
+This assignment required me to fetch server-side APIs to fetch city coordinate and weather data to display the weather forecast of my chosen city. The assignment also required that I store the user's search history in local data which then generates a button on the pages next visit. The user's searches are also stored in dynamically created buttons that they can then click on to view that forecast.
+
+## Tasks
+
+- Used GeoCode API to grab coordinates based on the city that the user input.
+- Used OpenWeather API to display a five day forecast based on the coordinates grabbed by the GeoCode API
+- created a function to store user inputs into the local storage, local storage inputs are then recovered and used to create dynamic buttons with previous user input data
+- created an event listener that prompts user for their location and displays their local weather.
+- used unsplash to display dynamic backgrounds.
